88d81eaf268d29d3bcbe2a5954df67d5d326148d
Sizhe Yuen <sizhe1007@gmail.com>
2018-06-18 12:08:22 +0100

    Add rust, markdown modes and refresh package contents on init

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ; Init package
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -10,6 +9,7 @@
 			 ("popkit" . "http://elpa.popkit.org/packages") ;; For Chinese firewall
 			 ("org" . "http://orgmode.org/elpa")))
 (package-initialize)
+(package-refresh-contents) ;; Refresh package contents. TODO: run conditionally
 
 
 ;; Init use-package if not installed
@@ -29,3 +29,17 @@
 
 ;;(require 'diminish)
 (require 'bind-key)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (markdown-mode markdown-mode+ yaml-mode rust-mode use-package pallet neotree ivy haskell-mode flycheck evil enh-ruby-mode elfeed company))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
