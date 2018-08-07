70023c993b544d59b5ddd9735e4c619b3cfc356f
Sizhe Yuen <sizhe1007@gmail.com>
2018-08-07 09:50:51 +0100

    Remove marmalade from repo list

--- init.el
+++ init.el
@@ -4,7 +4,6 @@
 
 (setq package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")
 			 ("melpa" . "https://melpa.org/packages/")
-			 ("marmalade" . "https://marmalade-repo.org/packages/")
 			 ("gnu" . "https://elpa.gnu.org/packages/")
 			 ("popkit" . "http://elpa.popkit.org/packages") ;; For Chinese firewall
 			 ("org" . "http://orgmode.org/elpa")))
@@ -34,9 +33,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" default)))
  '(package-selected-packages
    (quote
-    (evil-magit evil-numbers protobuf-mode magit markdown-mode markdown-mode+ yaml-mode rust-mode use-package pallet neotree ivy haskell-mode flycheck evil enh-ruby-mode elfeed company))))
+    (color-theme-monokai monokai-theme speed-type evil-magit evil-numbers protobuf-mode magit markdown-mode markdown-mode+ yaml-mode rust-mode use-package pallet neotree ivy haskell-mode flycheck evil enh-ruby-mode elfeed company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
